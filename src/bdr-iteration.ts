23babb2b448fb475145704f5bd8f8af65c2fdbb0
Krisna Rusdiono <krisnarusdiono@gmail.com>
2024-11-05 22:09:36 +0700

    feat: add example of behaviour-pattern iterator

---
+++ src/bdr-iteration.ts
@@ -0,0 +1,56 @@
+class Book {
+    constructor(public title: string, public author: string) {}
+}
+
+// Define Book Iterator
+class BookIterator {
+    private current: number = 0;
+    private collection: BookCollection;
+
+    constructor(collection: BookCollection) {
+        this.collection = collection;
+    }
+
+    hasNext(): boolean {
+        return this.current < this.collection.getBooks().length;
+    }
+
+    next(): Book {
+        return this.collection.getBooks()[this.current++];
+    }
+}
+
+// Define Book Collection
+// BookCollection.ts
+class BookCollection {
+    private books: Book[] = [];
+
+    addBook(book: Book): void {
+        this.books.push(book);
+    }
+
+    getIterator(): BookIterator {
+        return new BookIterator(this);
+    }
+
+    getBooks(): Book[] {
+        return this.books;
+    }
+}
+
+// Client code
+function main() {
+    const bookCollection = new BookCollection();
+    bookCollection.addBook(new Book("The Great Gatsby", "F. Scott Fitzgerald"));
+    bookCollection.addBook(new Book("1984", "George Orwell"));
+    bookCollection.addBook(new Book("To Kill a Mockingbird", "Harper Lee"));
+
+    const iterator = bookCollection.getIterator();
+
+    while (iterator.hasNext()) {
+        const book = iterator.next();
+        console.log(`Title: ${book.title}, Author: ${book.author}`);
+    }
+}
+
+main();
